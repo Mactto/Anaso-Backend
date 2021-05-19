9628d9feb3998c32bb556228c833dcb6c3726dc9
YulHee Kim <dbfgml980413@gmail.com>
2021-05-19 23:14:42 +0900

    개발:: 프로젝트 모델 설계 (율희)

---
+++ models/Project.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const projectSchema = mongoose.Schema({
+    title: {
+        type: String,
+        maxlength: 100
+    },
+    participant: {
+        type: String
+    },
+    startDate: {
+        type: Date,
+        default: Date.now()
+    },
+    endDate: {
+        type: Date,
+    },
+    thumbnail: {
+        type: String
+    },
+    role: {
+        type: String
+    },
+    explanation: {
+        type: String,
+        maslength: 1000
+    },
+})
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = { Project }
